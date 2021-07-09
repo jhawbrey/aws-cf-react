bc57fde4b7836eb6f45509abef38370c620ff45b
Jason Awbrey <jawbrey@gmail.com>
2021-07-09 17:14:47 -0500

    update infrastructure setup for CDN

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,25 +1,30 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-import * as iam from '@aws-cdk/aws-iam';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, 'WebsiteBucket', {
-      bucketName: 'jason-awbrey-cdk-blog', // Choose your own bucket name here
-      websiteIndexDocument: 'index.html', // 1
-      blockPublicAccess: new s3.BlockPublicAccess({ restrictPublicBuckets: false }) // 2
+      bucketName: 'jason-awbrey-cdk-blog',
+      websiteIndexDocument: 'index.html',
     });
 
-    // 3
-    const bucketPolicy = new iam.PolicyStatement({
-      actions: ['s3:GetObject'],
-      resources: [
-        `${bucket.bucketArn}/*`
-      ],
-      principals: [new iam.Anyone()],
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, 'OAI');
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'MyDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: cloudFrontOAI,
+          },
+          behaviors: [{ isDefaultBehavior: true }]
+        }
+      ]
     })
-    bucket.addToResourcePolicy(bucketPolicy); // 4
+
+    bucket.grantRead(cloudFrontOAI.grantPrincipal);
   }
 }
